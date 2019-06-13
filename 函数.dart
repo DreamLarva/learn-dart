90edc8a84205192c47a3230e7f0b2481ea54959e
Agent47 <654136828@qq.com>
2019-06-13 15:38:44 +0800

    2019/6/13

--- 函数.dart
+++ 函数.dart
@@ -1,3 +1,11 @@
+import 'package:meta/meta.dart';
+
+/**
+ * 每个app 想要可执行 就必须要有一个 main 函数
+ * main 函数 返回类型 一定是 void
+ * 可以使用 args 库 给main 传入一个参数 void main(List<String> arguments) {}
+ * */
+bool topLevel = true;
 void main() {
   /**
    * dart 中函数 和 js 相同 都是一等公民 且是 对象
@@ -46,8 +54,7 @@ void main() {
      * 需要 meta 库的依赖
      * import 'package:meta/meta.dart';
      * */
-    // Scrollbar({bool key, @required bool child}) {}
-
+    Scrollbar({bool key, @required bool child}) {}
   }
   /**
    * 可选基于位置的参数
@@ -99,19 +106,132 @@ void main() {
 
     assert(say('Bob', 'Howdy') == 'Bob says Howdy with a carrier pigeon');
 
-     // foo(bool a,[bool b = a]){} // error 默认值必须是常量
+    // foo(bool a,[bool b = a]){} // error 默认值必须是常量
 
 
     // 可以是用 各种类型的参数
-    void doStuff(
-        {List<int> list = const [1, 2, 3],
-          Map<String, String> gifts = const {
-            'first': 'paper',
-            'second': 'cotton',
-            'third': 'leather'
-          }}) {
+    void doStuff({List<int> list = const [1, 2, 3],
+      Map<String, String> gifts = const {
+        'first': 'paper',
+        'second': 'cotton',
+        'third': 'leather'
+      }}) {
       print('list:  $list');
       print('gifts: $gifts');
     }
   }
+
+  /**
+   * 函数是 一等公民
+   * 可以作为 参数
+   * */
+  {
+    void printElement(int element) {
+      print(element);
+    }
+
+    var list = [1, 2, 3];
+
+    // Pass printElement as a parameter.
+    list.forEach(printElement);
+
+    var loudify = (msg) => '!!! ${msg.toUpperCase()} !!!';
+    assert(loudify('hello') == '!!! HELLO !!!');
+  }
+  /**
+   * 匿名函数(又为 lambda 或 闭包(closure))
+   * 和 js 完全相同
+   * */
+  {
+    var list = ['apples', 'bananas', 'oranges'];
+    list.forEach((item) {
+      print('${list.indexOf(item)}: $item');
+    });
+
+    list.forEach((item) => print('${list.indexOf(item)}: $item'));
+  }
+  /**
+   * 作用域
+   * 和 js 完全相同
+   * */
+  {
+    var insideMain = true;
+
+    void myFunction() {
+      var insideFunction = true;
+
+      void nestedFunction() {
+        var insideNestedFunction = true;
+
+        assert(topLevel);
+        assert(insideMain);
+        assert(insideFunction);
+        assert(insideNestedFunction);
+      }
+    }
+    myFunction();
+  }
+  /**
+   * 闭包
+   * 和js 相同
+   * */
+  {
+    // 函数的返回值 为一个函数
+    Function makeAdder(num addBy) {
+      return (num i) => addBy + i;
+    }
+    // Create a function that adds 2.
+    var add2 = makeAdder(2);
+
+    // Create a function that adds 4.
+    var add4 = makeAdder(4);
+
+    assert(add2(3) == 5);
+    assert(add4(3) == 7);
+  }
+  /**
+   * 测试函数是否相等
+   * */
+  {
+    void foo() {}
+
+    var x;
+
+    x = foo;
+    assert(foo == x);
+
+    // Comparing static methods.
+    x = A.bar;
+    assert(A.bar == x);
+
+
+    var v = A(); // Instance #1 of A
+    var w = A(); // Instance #2 of A
+    var y = w;
+    x = w.baz;
+
+    // These closures refer to the same instance (#2),
+    // so they're equal.
+    assert(y.baz == x);
+
+    // 对比相同原型的不同实例上的方法
+    // 不相等(不基于原型链? js 中是相等的)
+    assert(v.baz != w.baz);
+  }
+  /**
+   * 返回值
+   * 所有函数的 隐式返回 默认值是 null
+   * */
+  {
+    foo() {}
+
+    assert(foo() == null);
+  }
+
 }
+
+class A {
+  static void bar() {} // A static method
+  void baz() {} // An instance method
+}
+
